e88e985a92e21eb50ea874d3c68ef5a579174f48
vpetrakis <123031812+vpetrakis@users.noreply.github.com>
2026-05-13 10:50:29 +0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,19 +1,12 @@
 from .parser import parse_workbook
 from .metrics import (
-    pipeline_summary,
-    status_distribution,
-    category_breakdown,
-    supplier_performance,
-    timeline_data,
-    delayed_items,
+    pipeline_summary, status_distribution, category_breakdown,
+    supplier_performance, timeline_data, delayed_items,
+    age_distribution, cost_by_month,
 )
-
 __all__ = [
     "parse_workbook",
-    "pipeline_summary",
-    "status_distribution",
-    "category_breakdown",
-    "supplier_performance",
-    "timeline_data",
-    "delayed_items",
+    "pipeline_summary", "status_distribution", "category_breakdown",
+    "supplier_performance", "timeline_data", "delayed_items",
+    "age_distribution", "cost_by_month",
 ]
